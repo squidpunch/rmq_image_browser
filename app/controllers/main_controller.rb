6f9adc209a42cd2ce978b327ddf82661f485886c
David Larrabee <david.larrabee@meyouhealth.com>
2014-04-28 12:01:49 -0400

    Wires up searching functionality

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -15,11 +15,35 @@ class MainController < UIViewController
     @query = rmq.append(UITextField, :query).focus.get
 
     rmq.append(UIButton, :submit_button).on(:touch) do |sender|
-      puts 'button tapped'
+      search_for_images @query.text
     end
   end
 
   def init_nav
     self.title = 'Image Browser'
+  end
+
+  def search_for_images(query)
+    if query && query != ''
+      query = query.gsub(/\s/, "%20")
+      url = "https://secure.flickr.com/search/?q=#{query}&s=int"
+
+      rmq.animations.start_spinner
+
+      AFMotion::HTTP.get(url) do |result|
+        if html = result.body
+          images = html.scan(/src=\"(.+?\.jpg)\"/).map do |m|
+            m.first
+          end
+        end
+        open_images_controller(images) if images.length > 0
+        rmq.animations.stop_spinner
+      end
+    end
+  end
+
+
+  def open_images_controller(images)
+    puts 'do it'
   end
 end
